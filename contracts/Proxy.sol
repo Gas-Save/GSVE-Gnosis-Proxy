8de1e43dd63511195fd5e3ecf00153b18744882d
GSVE Anon <anon@gassave.org>
2021-05-11 12:42:12 +0100

    added proxy

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -1,8 +1,18 @@
 pragma solidity ^0.6.3;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IGasToken {
+    /**
+     * @dev return number of tokens freed up.
+     */
+    function freeFromUpTo(address from, uint256 value) external returns (uint256); 
+
+}
 /// @title Proxy - Generic proxy contract allows to execute all transactions applying the code of a master contract.
 /// @author Stefan George - <stefan@gnosis.io>
 /// @author Richard Meissner - <richard@gnosis.io>
+/// @author Gas Save Protocol - GasSave.org
 contract Proxy {
 
     // masterCopy always needs to be first declared variable, to ensure that it is at the same location in the contracts to which calls are delegated.
@@ -18,10 +28,59 @@ contract Proxy {
         masterCopy = _masterCopy;
     }
 
+
+    /**
+    * @dev GSVE moddifier that burns W/GST2 or W/Chi 
+    * burn happens if the caller has provided the gnosis proxy with gas token allowances
+    * No comparing of W/GST1 as this adds way too much complexity and overhead!
+    * Probably a more elegant way of doing this, but it works ;)
+    */
+    modifier discountGas(bool discount) {
+        uint256 gasStart = gasleft();
+        _;
+        address token1;
+        uint256 ammount1; 
+        address token2;
+        uint256 ammount2; 
+
+        //compare CHI vs GST2
+        (token1, ammount1) = compareAllowance(0x0000000000004946c0e9F43F4Dee607b0eF1fA1c, 0x0000000000b3F879cb30FE243b4Dfee438691c04);
+        
+        //compare wchi vs wgst2
+        (token2, ammount2) = compareAllowance(0x7738c2a90eed0d3df85b80ffe5867e56eb7d7953, 0x6cae6b3487558944d5902bbf74502877265f5430);
+
+        address token;
+        uint256 amount; 
+        (token, amount) = compareAllowance(token1, token2);
+
+        if(amount > 0){
+            uint256 gasSpent = 21000 + gasStart - gasleft() + 16 * msg.data.length;
+            IFreeFromUpTo(token).freeFromUpTo(msg.sender,  (gasSpent + 16000) / 24000);
+        }
+    }
+
+    /**
+    * @dev GSVE moddifier that burns W/GST2 or W/Chi 
+    * burn happens if the caller has provided the gnosis proxy with gas token allowances
+    * No comparing of W/GST1 as this adds way too much complexity and overhead!
+    * Probably a more elegant way of doing this, but it works ;)
+    */
+    function compareAllowance(address one, address two) internal returns (address token, uint256 value) {
+        uint256 allowanceOne = IERC20(one).allowance(msg.sender, address(this));
+        uint256 allowanceTwo = IERC20(two).allowance(msg.sender, address(this)); 
+        if(allowanceOne > allowanceTwo) {
+            return (one, allowanceOne);
+        }
+        else{
+            return (two, allowanceTwo);
+        }
+    }
+
     /// @dev Fallback function forwards all transactions and returns all received return data.
-    function ()
+    function () 
         external
         payable
+        discountGas(true)
     {
         // solium-disable-next-line security/no-inline-assembly
         assembly {
